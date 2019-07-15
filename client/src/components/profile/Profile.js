630b13360e3c433e6541d47f788876bfc5b0c5f2
rui li <rli925@usc.edu>
2019-07-15 16:09:44 -0700

    Profile Details 2: Top & About

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop.js';
+import ProfileAbout from './ProfileAbout.js';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -25,8 +27,13 @@ const Profile = ({
             {auth.isAuthenticated 
             && auth.loading === false 
             && auth.user._id === profile.user._id 
-            && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
-        </Fragment> 
+            && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)} 
+            <div className='profile-grid my-1'>
+                <ProfileTop profile={profile} />
+                <ProfileAbout profile={profile} />
+                
+            </div>
+        </Fragment>
     )}
     </Fragment>
   );
