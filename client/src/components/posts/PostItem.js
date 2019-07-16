aa78a01dfcf5a38c3f279bdf4d9930f7bd944833
rui li <rli925@usc.edu>
2019-07-15 18:36:27 -0700

    Posts & Comments: Deleting Posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import {addLike, removeLike} from '../../actions/post';
+import {addLike, removeLike, deletePost} from '../../actions/post';
 
 const PostItem = ({
     addLike,
     removeLike,
+    deletePost,
     auth,
     post: { _id, text, name, avatar, user, likes, comments, date }
 }) => (
@@ -35,7 +36,7 @@ const PostItem = ({
                 )}
                 </Link>
                 {!auth.loading && user === auth.user._id && (
-                <button type='button' className='btn btn-danger'><i className='fas fa-times' /></button>
+                <button type='button' className='btn btn-danger' onClick={()=>deletePost(_id)}><i className='fas fa-times' /></button>
                 )}
             </Fragment>
         
@@ -44,8 +45,9 @@ const PostItem = ({
 );
 
 PostItem.propTypes = {
-    addLike: PropTypes.object.isRequired,
-    removeLike: PropTypes.object.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired,
     post: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired,
 };
@@ -54,4 +56,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps,{addLike, removeLike})(PostItem);
+export default connect(mapStateToProps,{addLike, removeLike, deletePost})(PostItem);
