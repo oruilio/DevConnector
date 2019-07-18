84cc22a5a85d7876c6650d23b8f3051d6706b4de
rui li <rli925@usc.edu>
2019-07-18 11:57:02 -0700

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -27,12 +27,10 @@ const App = () =>{
       <Router>
         <Fragment>
           <Navbar />
-            <Switch>
-              <Route exact path= '/' component={Landing} />
-              <Route component={Routes} />
-            </Switch>
-          <Route exact path= '/' component={Landing} />
-          <Route component={Routes} />
+          <Switch>
+            <Route exact path= '/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment> 
       </Router>
     </Provider>
